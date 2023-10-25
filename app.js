90fdcea057b7d5e7321c9e90e5345627e2307873
Luis <luis.ortiz.rivera@alumnos.upm.es>
2023-10-25 16:04:34 +0300

    Added DB config and connection logs

--- app.js
+++ app.js
@@ -8,12 +8,28 @@ const mongoose = require("mongoose");
 const app = express();
 const PORT = 3000;
 const users = require("./routes/users");
+const dbConfig = require("./config/database");
+
+// #region Database config
+mongoose.connect(dbConfig.database);
+
+mongoose.connection.on("connected", () => {
+	console.log("Connected to database: " + dbConfig.database);
+});
+
+mongoose.connection.on("error", (err) => {
+	console.log("Something failed while connecting to the DB: " + err);
+});
+// #endregion
 
 //Allow outside domain calls
 app.use(cors());
 
 //Body parser middleware
 app.use(bodyParser.json());
+
+//Set static folder
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
 	res.send("<h1>Hello world!</h1>");
